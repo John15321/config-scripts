789a3d1abed08d29b38b4826120ae8e6bf4cdff4
John15321 <janbronicki@gmail.com>
2021-02-05 17:48:01 +0100

    .

--- zsh_install_4.sh
+++ zsh_install_4.sh
@@ -9,3 +9,5 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/p
 
 # plugins=(git zsh-autosuggestions zsh-syntax-highlighting)
 # https://www.tecmint.com/install-oh-my-zsh-in-ubuntu/
+
+# in vscode change font to FuraMono Nerd Font
